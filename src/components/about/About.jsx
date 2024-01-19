0f0fa33c28a33330063e07740de1acee087ccd8d
Josh <j.prestage@hotmail.com>
2024-01-19 20:34:17 +0000

    About Me jsx structure

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
-import './about.css'
+import React from "react";
+import "./about.css";
+import me from "../../assets/me2.png";
+import { FaAward } from "react-icons/fa6";
+import { FiUsers } from "react-icons/fi";
+import { FaFolderOpen } from "react-icons/fa";
 
 const About = () => {
   return (
-    <section id='about' >About</section>
-  )
-}
+    <section id="about">
+      <h5>Get To Know Me</h5>
+      <h2>About Me</h2>
 
-export default About
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me-image">
+            <img src={me} alt="me" className="me2_img" />
+          </div>
+        </div>
+        <div className="about_content">
+          <div className="about_cards">
+            <article className="about_card">
+              <FaAward className="about_icon" />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+            <article className="about_card">
+              <FiUsers className="about_icon" />
+              <h5>Clients</h5>
+              <small>10+ Clients</small>
+            </article>
+            <article className="about_card">
+              <FaFolderOpen className="about_icon" />
+              <h5>Projects</h5>
+              <small>7+ Completed PRojects</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+            sequi aut ratione sapiente fugit voluptas sed facere consectetur
+            quod aperiam nam qui, eius reprehenderit, tempore aliquid veniam at,
+            placeat similique!
+          </p>
+
+          <a href="#contact" className="btn btn-primary" >Lets Connect</a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import {BsLinkedin} from'react-icons/bs'
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
 import { FaSquareGithub } from "react-icons/fa6";
 
 const HeaderSocials = () => {
   return (
-    <div className='header_socials' >
-      <a href="https://www.linkedin.com/in/josh-prestage/" target='_blank'><BsLinkedin/></a>
+    <div className="header_socials">
+      <a href="https://www.linkedin.com/in/josh-prestage/" target="_blank">
+        <BsLinkedin />
+      </a>
 
-      <a href="https://github.com/JoshP369" target='_blank'><FaSquareGithub /></a>
+      <a href="https://github.com/JoshP369" target="_blank">
+        <FaSquareGithub />
+      </a>
+    </div>
+  );
+};
 
-       </div>
-  )
-}
-
-export default HeaderSocials 
+export default HeaderSocials;
