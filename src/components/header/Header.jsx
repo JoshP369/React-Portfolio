047e3cb2001ce1269b458691a7bc1277cc14505f
Josh <j.prestage@hotmail.com>
2024-01-19 19:31:09 +0000

    Floating nav complete

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header_container">
         <h5>Hello I'm</h5>
         <h1>Josh Prestage</h1>
-        <h5 className="text-light"> Frontend Web Developer</h5>
+        <h5> Frontend Web Developer</h5>
         <CTA />
         <HeaderSocials />
         <div className="me">
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,17 +5,21 @@ import { FaUserAstronaut } from "react-icons/fa";
 import { FaCode } from "react-icons/fa";
 import { GiGiftOfKnowledge } from "react-icons/gi";
 import { TiMessages } from "react-icons/ti";
+import { useState } from 'react';
+
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#');
   return (
     <nav>
-      <a href="#"><FaHome /></a>
-      <a href="#about"><FaUserAstronaut /></a>
-      <a href="#experience"><FaCode /></a>
-      <a href="#portfolio"><GiGiftOfKnowledge /></a>
-      <a href="#contact"><TiMessages /></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' :""} ><FaHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' :""} ><FaUserAstronaut /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' :""} ><FaCode /></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' :""}><GiGiftOfKnowledge /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' :""}><TiMessages /></a>
     </nav>
   )
 }
 
 export default Nav
+
